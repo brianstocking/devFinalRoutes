2754fb8539f0fdf890074f7e4467675de3a01d3c
Brian Stocking <brianm.stocking@gmail.com>
2015-03-11 18:28:06 -0600

    working on charting

--- js/service.js
+++ js/service.js
@@ -2,8 +2,8 @@ var app = angular.module('poopApp');
 
 app.service('mainService', function(){
     var numberTimes = 0;
-    this.date = new Date().toDateString()
-
+    //this.date = new Date().toDateString()
+this.date = "Mon Mar 09 2015"
 
     this.getNumberTimes = function () {
         return numberTimes;
